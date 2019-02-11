be4a8c53603df7dd816b01a842a40582bf6bcd44
Alessandro Ferreira <alessandrosferreira@gmail.com>
2019-02-11 12:39:49 -0200

    start

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const PORT = 3000;
+const HOST = "0.0.0.0";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello world! Olá");
+});
+
+app.listen(PORT, HOST, () => {
+  console.log("Servidor online");
+});
